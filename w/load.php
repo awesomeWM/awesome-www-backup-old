0992dfe9920d2fbbb678642a9debc4fb14a26161
Uli Schlachter <psychon@znc.in>
2016-05-18 18:17:27 +0200

    Import the web page

---
+++ w/load.php
@@ -0,0 +1,3 @@
+/* This file is the Web entry point for MediaWiki's ResourceLoader:
+   <https://www.mediawiki.org/wiki/ResourceLoader>. In this request,
+   no modules were requested. Max made me put this here. */
